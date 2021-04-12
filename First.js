fd9ab5da89b8a8f1500e8ace77b66d5a894ce4f5
BLaineLaas <blainelaas.bl@gmail.com>
2021-04-12 20:04:11 +0200

    Created first program, added object and for loop

---
+++ First.js
@@ -0,0 +1,18 @@
+// declare an object
+let Lillian = {
+
+    daughter: 'Emma',
+    boyfriend: 'Tom',
+    mother: 'Jess',
+    sister: 'susan'
+
+};
+
+// create for loop
+let element;
+for (element of Lillian.daughter){
+
+    console.log(element);
+}
+
+console.log(element);
